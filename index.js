c5725cbc5a7394da964e1e64345a130de235294f
Essence Alexander <upperlefty@zoho.eu>
2017-12-05 16:04:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 <script src="http://www.clocklink.com/embed.js"></script>
 <script type="text/javascript" language="JavaScript">obj=new 
   Object;obj.clockfile="world001-green.swf";
-  obj.TimeZone="GMT0530";obj.width=480;obj.height=250;obj.wmode="transparent";showClock(obj);</script>
+  obj.TimeZone="GMT0530";obj.width=480;obj.height=250;obj.wmode="transparent";
+    showClock(obj);</script>
 
