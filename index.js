2bb5edd2270db326770a6591662115940310f5b4
Essence Alexander <upperlefty@zoho.eu>
2017-12-05 16:02:29 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+<script src="http://www.clocklink.com/embed.js"></script>
+<script type="text/javascript" language="JavaScript">obj=new Object;obj.clockfile="world001-green.swf";obj.TimeZone="GMT0530";obj.width=480;obj.height=250;obj.wmode="transparent";showClock(obj);</script>
+
